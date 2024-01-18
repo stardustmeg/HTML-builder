085a1280f5dd61a70ac598e55946605358fc35e6
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-01-18 23:13:45 +0300

    feat: solve 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
+const { extname, basename } = require('node:path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+(async () => {
+  try {
+    const files = await fsPromises.readdir(folderPath);
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const stats = await fsPromises.stat(filePath);
+
+      if (stats.isFile()) {
+        const fileName = basename(file, extname(file));
+        const fileExtension = file.split('.').pop();
+        const fileSize = `${stats.size}B`;
+
+        console.log(`${fileName} - ${fileExtension} - ${fileSize}`);
+      }
+    }
+  } catch (err) {
+    console.error(`Something went wrong! ${err}`);
+  }
+})();
